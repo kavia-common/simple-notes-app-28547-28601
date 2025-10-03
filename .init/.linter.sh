56d5882e6a30ac084f4cd7bc08deecd4cf7a0e96
Kavia AI <production@kavia.ai>
2025-10-03 09:07:41 +0000

    CheckPoint - cg7a6ddec6

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/simple-notes-app-28547-28601/notes_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
